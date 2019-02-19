554ceaa7a6be96786c7ebcee327ad6832f98926f
spartanrex10 <46965967+spartanrex10@users.noreply.github.com>
2019-02-19 09:04:19 -0600

    Update GuessingGame.java
    I made the entirety of the code. It just needs error checking.

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,10 +1,29 @@
-
+import java.util.Scanner;
+import java.lang.Math;
 public class GuessingGame {
 
     public static void main(String[] args) {
-    
+        Scanner scan = new Scanner(System.in);
         System.out.println("Welcome to the guessing game.");
-    
+        System.out.println("Please put in the range of numbers to choose from:");
+        int x = scan.nextInt();
+        int y = (int)(Math.random() * x);
+        int z = -20;
+        System.out.println("Guess a number between 0 & " + x);
+        //This is the guessing game
+        for (int i = 1; z != y; i++) {
+          System.out.println("Input you guess:");
+          z = scan.nextInt();
+          if (z>y) {
+            System.out.println("You're too high.");
+          }
+          if (z<y) {
+            System.out.println("You're too low.");
+          }
+          if (z == y) {
+            System.out.println("You got it right! You guessed: " + i + " times.");
+          }
+        }
+        scan.close();
     }
-
 }
