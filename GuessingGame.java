4624cec3a402966171c2595f0e70441c6f52df80
spartanrex10 <46965967+spartanrex10@users.noreply.github.com>
2019-02-20 18:48:16 -0600

    Added error checking to the guessing game.

--- GuessingGame.java
+++ GuessingGame.java
@@ -6,6 +6,11 @@ public class GuessingGame {
         Scanner scan = new Scanner(System.in);
         System.out.println("Welcome to the guessing game.");
         System.out.println("Please put in the range of numbers to choose from:");
+        //This checks that the input is an integer
+        while (!scan.hasNextInt()) {
+          System.out.println("Invalid input");
+          scan.next();
+        }
         int x = scan.nextInt();
         int y = (int)(Math.random() * x);
         int z = -20;
@@ -13,7 +18,23 @@ public class GuessingGame {
         //This is the guessing game
         for (int i = 1; z != y; i++) {
           System.out.println("Input you guess:");
+          //This checks that the input is an integer
+          while (!scan.hasNextInt()) {
+          System.out.println("Invalid input");
+          scan.next();
+          }
+
           z = scan.nextInt();
+          //This checks it's not above or below x
+          while (z>x || z<0) {
+          System.out.println("That number is out of bounds");
+          //This checks that the input is an integer
+          while (!scan.hasNextInt()) {
+          System.out.println("Invalid input");
+          scan.next();
+          }
+          z = scan.nextInt();
+          }
           if (z>y) {
             System.out.println("You're too high.");
           }
